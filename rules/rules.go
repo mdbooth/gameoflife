e14a78c47a4a3e25033bbac3129680308588a402
Matthew Booth <matthew@heisenbug.com>
2020-04-26 19:58:25 +0100

    Add some example GoL rules

--- rules/rules.go
+++ rules/rules.go
@@ -1,9 +1,5 @@
 package rules
 
-import (
-	"math/rand"
-)
-
 const (
 	BOARD_WIDTH  = 100
 	BOARD_HEIGHT = 100
@@ -14,6 +10,42 @@ type Board struct {
 	next   *Board
 }
 
+func nNeighbours(board *Board, x, y int) int {
+	lowerLimit := func(n, limit int) int {
+		if n < limit {
+			return limit
+		}
+		return n
+	}
+
+	upperLimit := func(n, limit int) int {
+		if n > limit {
+			return limit
+		}
+		return n
+	}
+
+	xLower := lowerLimit(x-1, 0)
+	xUpper := upperLimit(x+1, BOARD_WIDTH-1)
+	yLower := lowerLimit(y-1, 0)
+	yUpper := upperLimit(y+1, BOARD_HEIGHT-1)
+
+	n := 0
+	for i := xLower; i <= xUpper; i++ {
+		for j := yLower; j <= yUpper; j++ {
+			if x == i && y == j {
+				continue
+			}
+
+			if board.Pieces[i][j] {
+				n++
+			}
+		}
+	}
+
+	return n
+}
+
 func NewBoard() *Board {
 	var a, b Board
 	a.next = &b
@@ -22,11 +54,27 @@ func NewBoard() *Board {
 }
 
 func UpdateBoard(board *Board) *Board {
-	board.next.Pieces = board.Pieces
+	next := board.next
+	for x := 0; x < BOARD_WIDTH; x++ {
+		for y := 0; y < BOARD_HEIGHT; y++ {
+			n := nNeighbours(board, x, y)
+			if board.Pieces[x][y] {
+				// Under or over population
+				if n < 2 || n > 3 {
+					next.Pieces[x][y] = false
+				} else {
+					next.Pieces[x][y] = true
+				}
+			} else {
+				// Reproduction
+				if n == 3 {
+					next.Pieces[x][y] = true
+				} else {
+					next.Pieces[x][y] = false
+				}
+			}
+		}
+	}
 
-	xRand := rand.Intn(BOARD_WIDTH)
-	yRand := rand.Intn(BOARD_HEIGHT)
-
-	board.next.Pieces[xRand][yRand] = true
-	return board.next
+	return next
 }
